cf34bc8b8a7bbc5102e85514725e53460c3adaf2
unknown <matthew.tomitz@gmail.com>
2019-05-16 21:36:54 -0400

    adding tables

---
+++ dbo.Contacts.sql
@@ -0,0 +1,11 @@
+﻿CREATE TABLE [dbo].[Contacts] (
+    [AccountId]   INT           IDENTITY (1, 1) NOT NULL,
+    [Id]          INT           NULL,
+    [FirstName]   NVARCHAR (50) NULL,
+    [LastName]    NVARCHAR (50) NULL,
+    [Address]     NVARCHAR (50) NULL,
+    [PhoneNumber] NVARCHAR (50) NULL,
+    [DateCreated] DATETIME      NULL,
+    PRIMARY KEY CLUSTERED ([AccountId] ASC)
+);
+
---
+++ dbo.UserTable.sql
@@ -0,0 +1,8 @@
+﻿CREATE TABLE [dbo].[UserTable] (
+    [Id]           INT           IDENTITY (1, 1) NOT NULL,
+    [UserName]     NVARCHAR (50) NULL,
+    [PasswordHash] NVARCHAR (50) NULL,
+    [DateCreated]  DATETIME      DEFAULT (getdate()) NULL,
+    CONSTRAINT [PK_UserTable] PRIMARY KEY CLUSTERED ([Id] ASC)
+);
+
